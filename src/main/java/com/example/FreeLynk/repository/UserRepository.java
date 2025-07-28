357e24bf98d98b8ff147e03dbd1ee5dc1303dda3
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2025-07-28 23:27:19 +0530

    Add method to find User by email in UserRepository

--- src/main/java/com/example/FreeLynk/repository/UserRepository.java
+++ src/main/java/com/example/FreeLynk/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.FreeLynk.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.FreeLynk.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
-    
+    Optional<User> findByEmail(String email);
 }
