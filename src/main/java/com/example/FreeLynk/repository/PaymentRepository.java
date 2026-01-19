dbbf84be3dbfc93ddebbe6325fd5e04ed65d5a5b
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2026-01-19 14:20:46 +0530

    refactor payment repo to use UUID

--- src/main/java/com/example/FreeLynk/repository/PaymentRepository.java
+++ src/main/java/com/example/FreeLynk/repository/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.example.FreeLynk.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.example.FreeLynk.model.Payment;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment,Long> {
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     Optional<Payment> findByPaymentIntentId(String id);
 
 }
