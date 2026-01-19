d50b80a56ee352b1342902463820c8ece4ed5ca9
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2026-01-19 14:13:03 +0530

    refactor project repo to use UUID

--- src/main/java/com/example/FreeLynk/repository/ProjectRepository.java
+++ src/main/java/com/example/FreeLynk/repository/ProjectRepository.java
@@ -1,12 +1,14 @@
 package com.example.FreeLynk.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.FreeLynk.model.Project;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Project, UUID> {
     // Custom query methods can be defined here if needed
   
 
