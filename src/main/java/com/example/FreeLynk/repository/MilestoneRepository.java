73d3def3035618e4a55ccd6c512b9c17a9013ed4
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2026-01-19 14:19:31 +0530

    refactor Milestone repo to use UUID

--- src/main/java/com/example/FreeLynk/repository/MilestoneRepository.java
+++ src/main/java/com/example/FreeLynk/repository/MilestoneRepository.java
@@ -1,12 +1,13 @@
 package com.example.FreeLynk.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.FreeLynk.model.Milestone;
 
-public interface MilestoneRepository extends JpaRepository<Milestone, Long> {
-    List<Milestone> findByProjectId(Long projectId);
+public interface MilestoneRepository extends JpaRepository<Milestone, UUID> {
+    List<Milestone> findByProjectId(UUID projectId);
     
 }
