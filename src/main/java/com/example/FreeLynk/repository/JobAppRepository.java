c538de10cb5c9eb25d2d3277d8c0a476dcb0c6a5
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2026-01-19 14:19:48 +0530

    refactor job app repo to use UUID

--- src/main/java/com/example/FreeLynk/repository/JobAppRepository.java
+++ src/main/java/com/example/FreeLynk/repository/JobAppRepository.java
@@ -1,5 +1,7 @@
 package com.example.FreeLynk.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,6 @@ import com.example.FreeLynk.model.JobApplication;
 
 
 @Repository
-public interface JobAppRepository extends JpaRepository<JobApplication,Long> {
+public interface JobAppRepository extends JpaRepository<JobApplication, UUID> {
     
 }
