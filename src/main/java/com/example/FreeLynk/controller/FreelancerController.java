1299376caa4e7abbd3e62789a11be1f597dbed51
ris-jeewa <dheerasekararj.21@uom.lk>
2026-04-20 22:16:48 +0530

    Add admin authorization to getAllFreelancers and update createFreelancer method to use FreelancerCreateRequest

--- src/main/java/com/example/FreeLynk/controller/FreelancerController.java
+++ src/main/java/com/example/FreeLynk/controller/FreelancerController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.FreeLynk.dto.FreelancerCreateRequest;
 import com.example.FreeLynk.model.Freelancer;
 import com.example.FreeLynk.service.FreelancerService;
 
@@ -27,6 +29,7 @@ public class FreelancerController {
 
     // GET ALL FREELANCERS
     @GetMapping("")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<Freelancer>> getAllFreelancers() {
         List<Freelancer> freelancers = freelancerService.getAllFreelancers();
         return ResponseEntity.ok(freelancers);
@@ -50,8 +53,8 @@ public class FreelancerController {
 
     // CREATE FREELANCER
     @PostMapping("")
-    public ResponseEntity<Freelancer> createFreelancer(@RequestBody Freelancer freelancer) {
-        Freelancer createdFreelancer = freelancerService.createFreelancer(freelancer);
+    public ResponseEntity<Freelancer> createFreelancer(@RequestBody FreelancerCreateRequest request) {
+        Freelancer createdFreelancer = freelancerService.createFreelancerFromRequest(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdFreelancer);
     }
 
